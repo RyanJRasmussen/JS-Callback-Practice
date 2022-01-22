0475c3f1644237eef0a17288064bc20185a1b48b
RyanJRasmussen <87948132+RyanJRasmussen@users.noreply.github.com>
2022-01-21 19:16:28 -0600

    adding all changes

--- index.js
+++ index.js
@@ -1,8 +1,47 @@
 const inventory = newInventory()
 move(inventory).to(0, 0)
 
+//defines character image, and defines direction, which starts with no value 
+// defines x and y, allowing the two axes to be changed by altering these two variables
+
+
 const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
+
+function handleDirectionChange(direction){
+
+    if(direction === null){
+        character.src = 'assets/green-character/static.gif'
+    }
+    if(direction === 'west'){
+        character.src = 'assets/green-character/west.gif'
+    }
+    if(direction === 'north'){
+        character.src = 'assets/green-character/north.gif'
+    }
+    if(direction === 'east'){
+        character.src = 'assets/green-character/east.gif'
+    }
+    if(direction === 'south'){
+        character.src = 'assets/green-character/south.gif'
+    }
+
+}
+
+
+
+move(character).withArrowKeys(100, 250, handleDirectionChange)
+
+
+
+//establishes character image starting position
+
+
+// the commented-out line of code below this was causing the error 'callback is not a function' to be thrown
+// maybe because the move(character).to, using the to object, is clashing with the withArrowKeys object called ahead? 
+// the initial placement of 100, 250 is already established by the withArrowKets function on 32, so .to is redundant anyway
+
+
+// move(character).to(100, 250)
 
 
 move(newImage('assets/tree.png')).to(200, 450)
